59772b56401c4e5d38104983e0ff1f0658d63b15
jahongir1808 <jahongir.developer18@gmail.com>
2022-12-29 23:19:18 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+function openNavbar() {
+  document.getElementById("navbar-responsive").style.right = "-20%";
+  // document.getElementById("navbar-responsive").style.top = "0";
+}
+function closeNavbar() {
+  document.getElementById("navbar-responsive").style.right = "-100%";
+  // document.getElementById("navbar-responsive").style.top = "-100%";
+}
+
+window.addEventListener("scroll", function () {
+  shrink();
+});
+
+let navbar = document.getElementById("navbar");
+
+function shrink() {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    navbar.classList.add("navbar-shrink");
+  } else {
+    navbar.classList.remove("navbar-shrink");
+  }
+}
